f67aef6da7d971437f63dc35d45fc58c11466788
rabiulrafi <rabiulrafi01@gmail.com>
2020-05-05 00:59:10 +0600

    Template design + model Upadated

---
+++ store/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import *
+urlpatterns = [
+    path('',store,name='store'),
+    path('cart/',cart, name='cart'),
+    path('checkout/',checkout, name='checkout'),
+
+]
