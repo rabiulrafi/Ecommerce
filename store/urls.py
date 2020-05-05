2767caf39f7b723573466b828bf927fb42484afd
rabiulrafi <rabiulrafi01@gmail.com>
2020-05-05 19:00:04 +0600

    Update cart item

--- store/urls.py
+++ store/urls.py
@@ -4,5 +4,6 @@ urlpatterns = [
     path('',store,name='store'),
     path('cart/',cart, name='cart'),
     path('checkout/',checkout, name='checkout'),
+    path('update_item/',updateItem, name='updateItem'),
 
 ]
